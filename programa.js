d7f7159d1c15cae5305d71ba6fde5f03c8622c09
Staichak4 <pedro.staichaka.19@gmail.com>
2024-06-20 17:10:44 -0300

    Mudança no menu - Atividade em grupo :p

--- programa.js
+++ programa.js
@@ -8,5 +8,37 @@ const rl = readline.createInterface({
 executar()
 
 function executar() {
-
-}
+    console.log(` 
+    1. Adicionar lembrete
+    2. Listar lembretes
+    3. Editar lembretes
+    4. Marcar como concluída
+    5. Sair
+    `) 
+    
+ rl.question(`Escolha uma opção: `, (opcao) => {
+ 	switch (opcao) {
+ 	     case '1':
+ 	        adicionar()
+ 	        break;
+ 	     case '2':
+ 	        listar()
+ 	        break;
+ 	     case '3':
+ 	        editar()
+ 	        break;
+ 	     case '4':
+ 	        concluir()
+ 	        break;
+ 	     case '5':
+ 	        console.log("Fechando o programa!")
+ 	        rl.close()
+ 	        break;
+ 	     default:
+ 	        console.log('Opção inválida!')
+ 	        executar()
+ 	        break;
+ 	   
+ }
+ })
+ }
