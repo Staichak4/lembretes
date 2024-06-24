74e315c6680e56702efce525fab38bc65ba7e125
saci-capoeirista <acarloseduardo502@gmail.com>
2024-06-24 17:34:44 -0300

    implementacao do codigo adicionar

--- programa.js
+++ programa.js
@@ -36,14 +36,49 @@ rl.question(`Escolha uma opção: `, (opcao) => {
  	        break;
  	    default:
  	        console.log('Opção inválida!')
- 	        executar()
+ 	        exibirMenu()
  	        break;
  	   
  		}
  	})
- }
+}
+
+function adicionar(){
+	rl.question('Digite o lembrete: ', (lembrete) => {
+		rl.question('Digite o prazo: ', (prazo) => {
+			lembretes.push({ lembrete: lembrete, prazo: prazo})
+			console.log('Lembrete salvo com sucesso. ')
+		})
+	})
+	exibirMenu()
+}
 function listar () {
 	for (let i = 0; i < lembretes.length; i++ )
 		console.log(lembretes[i])
-}
+}	
+function editar(){
+	if(lembretes == 0){
+		console.log('sem lembretes registrados')
+		exibirMenu()
+	}else{
+		rl.question('digite o lembrete que deseja editar:', (opcao) =>{
+			if(opcao > 0 && opcao <= lembretes.length){
+				rl.question('digite o novo lembrete:',(lembrete) =>{
+				rl.question('digite um novo prazo: ', (prazo) =>{
+				rl.question('digite se já foi concluido: ', (concluido) => {
+                 lembretes[opcao - 1] = {
+					lembrete,
+					prazo,
+					concluido
+				 }
+				 console.log('editado com sucesso')
+				 exibirMenu()
+				})
+				})
+
+				})
+			}
+		})
+	}
 exibirMenu()
+}
